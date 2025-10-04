0a0f0475f36daf80ca354760e966d4785fbde4da
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2025-10-04 19:06:03 -0300

    feat: adicionar estrutura inicial do serviço de redirecionamento e gerenciador de shortlinks

---
+++ shortlink-manager-service/src/main/java/com/encurtador/shortlink_manager_service/ShortlinkManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.encurtador.shortlink_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortlinkManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortlinkManagerServiceApplication.class, args);
+	}
+
+}
---
+++ shortlink-manager-service/src/test/java/com/encurtador/shortlink_manager_service/ShortlinkManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtador.shortlink_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
