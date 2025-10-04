9a5b74995a8c2516c4923d396b87c264e4c1752f
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2025-10-04 19:05:36 -0300

    fix(gitignore): remove wildcard que ignorava short-link-key-service e adiciona pasta

---
+++ short-link-key-service/src/main/java/com/encurtaai/short_link_key_service/ShortLinkKeyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.encurtaai.short_link_key_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkKeyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortLinkKeyServiceApplication.class, args);
+	}
+
+}
---
+++ short-link-key-service/src/test/java/com/encurtaai/short_link_key_service/ShortLinkKeyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtaai.short_link_key_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkKeyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
